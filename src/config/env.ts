3bfe655e57e24102332b88d184a8a916221dedc6
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2026-09-08 01:57:23 +0300

    🐘 app is ready and running on port 4000

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,5 +7,6 @@ export const env = {
   nodeEnv: process.env.node_env ?? "development",
   isProduction: process.env.node_env === "production",
   logLevel: process.env.log_level ?? "info",
+  databaseUrl: process.env.database_url ?? "postgresql://postgres:postgres@localhost:5432/node_postgres_db",
 } as const;
 
