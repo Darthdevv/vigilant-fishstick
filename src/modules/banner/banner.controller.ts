a6eb558b214a64770cc248acc5668bc40cb0de43
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2026-09-22 10:13:41 +0300

    🐘 adding user and auth module

--- src/modules/banner/banner.controller.ts
+++ src/modules/banner/banner.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export const getBannerStatus = (_req: Request, res: Response) => {
-  res.status(200).json({ status: "ok", message: "Banner check passed" });
+  res.status(200).json({ status: "ok", message: "Banner status check passed" });
 };
 
